b611f8399db4719037f0a394e4f2cec6cbc01fc8
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-09 21:45:56 +0600

    created nested routes for different components

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,18 @@
 import {
     createBrowserRouter,
   } from "react-router-dom";
+import Main from "../Main/Main";
+import Home from "../components/Home/Home";
 
   export const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Main />,
+      children: [
+        {
+            path: '/', 
+            element: <Home />
+        }
+      ]
     },
   ]);
