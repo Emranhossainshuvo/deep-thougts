f4cfdadb7690c7dc968f7616b5f6a95d20238781
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-09 22:46:15 +0600

    completed the banner section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,12 +3,16 @@ const Banner = () => {
     return (
         <div className="max-w-screen-lg mx-auto">
             {/* section to hold the title and button  */}
-            <section className="flex justify-between items-center">
+            <section className="flex mb-11 justify-between items-center">
                 <h3 className="text-[#0029ff] text-2xl font-bold">Technical Project Management</h3>
                 <button className="p-2 mt-5 px-3 rounded-lg text-white bg-[#0029ff]">Submit task</button>
             </section>
 
             {/* section  */}
+            <section className="bg-[#e9ecef] p-4 text-black">
+                <h4 className="text-xl mb-3 font-bold">Explore the world of management</h4>
+                <p className="text-sm">As a project manager, you play an important reole in leading a project through initiation, planning, execution, monitoring, controlling and completion. How? Do you want to manage each and every step of your life?</p>
+            </section>
         </div>
     );
 };
